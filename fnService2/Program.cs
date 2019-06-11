032b241e2da9a764aa6f20f5f43c4750ac2ca241
Remi THOMAS <remi@xtware.com>
2019-06-11 16:27:38 +0200

    Create fnService2 that uses Topshelf as service manager.

---
+++ fnService2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace fnService2
+{
+    class Program
+    {
+        public class RunTask
+        {
+            FileNotify2.fnlib lib = new FileNotify2.fnlib();
+            public void Start()
+            {
+                new Thread(() => lib.Start()).Start();
+            }
+            public void Stop()
+            {
+                lib.Stop();
+            }
+        }
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<RunTask>(s =>
+                {
+                    s.ConstructUsing(name => new RunTask());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription("FileNotify2");
+                x.SetDisplayName("FileNotify2");
+                x.SetServiceName("FileNotify2");
+            });
+        }
+    }
+}
